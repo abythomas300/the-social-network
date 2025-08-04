9dd6f1d3e411cbde77d59be6125f16248fe0b271
abythomas300 <abythomas300@gmail.com>
2025-08-05 02:19:15 +0530

    feat(auth): add user registration

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,5 +7,24 @@ function displayRegistrationPage(req, res) {
     res.send('<h3 style="text-align:center; font-size: 2.5em; style=color:blue"><i>The Social Network</i></h3> <h5><u>Registration Page</u></h5>')
 }
 
+// method to handle user registration 
+async function registerUser(req, res) {
 
+    try {
+        // getting credentials from user's request
+        const username = req.body.username
+        const password = req.body.password
+
+        // encrypting password
+        const hashString = await bcrypt.hash(password, 10)   
+        const newUser = new userModel({username: username, password: hashString}) // creating user model object
+        await newUser.save()  // passing user model object to mongoose to create new document in DB
+        res.send("Registration Successful")
+    }
+    catch(error){
+        console.log("Registration Failed, reason: ", error)
+        res.send("Registration Failed")
+    }
+
+}
 
