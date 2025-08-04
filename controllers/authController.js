fa88851ab8fe2166a590dc1d61407d1268d11b20
abythomas300 <abythomas300@gmail.com>
2025-08-05 03:03:10 +0530

    style(registration page): add color to main header

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 
 // method to show registration page to user
 function displayRegistrationPage(req, res) {
-    res.send('<h3 style="text-align:center; font-size: 2.5em; style=color:blue"><i>The Social Network</i></h3> <h5><u>Registration Page</u></h5>')
+    res.send('<h3 style="text-align:center; font-size: 2.5em; color:blue"><i>The Social Network</i></h3> <h2><u>Registration Page</u></h2>')
 }
 
 // method to handle user registration 
