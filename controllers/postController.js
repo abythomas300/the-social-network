80e30f365e823edd26d5f615ec0f1dc7846fadc9
abythomas300 <abythomas300@gmail.com>
2025-08-09 23:38:09 +0530

    feat(posts): add functionality to view all posts on /post GET request

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,8 +1,19 @@
-function getAllPosts(req, res) {
-    res.send('<h3 style="text-align:center; font-size: 2.5em; color:blue"><i>The Social Network</i></h3> <p>All new posts will appear here, no new posts for now. </p>')
+// importing necessary modules
+const postModel = require('../models/post')
+const mongoose = require('mongoose')
+
+
+async function getAllPosts(req, res){
+
+    const allPosts = await postModel.find({})
+    console.log("Fetched Data -->", allPosts)
+    if(allPosts.length === 0){  // if there is no data in DB (ie. 'allPosts' is empty)
+        res.send('<h3 style="text-align:center; font-size: 2.5em; color:blue"><i>The Social Network</i></h3> <p>All posts will appear here, no new posts for now. </p>')
+    } else {
+        res.send(allPosts)
+    }
+
 }
-
-
 
 // exporting all methods
 module.exports = {
