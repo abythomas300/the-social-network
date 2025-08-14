a235a23804818782e6f60cbefc9c086af0f1e58d
abythomas300 <abythomas300@gmail.com>
2025-08-15 02:41:07 +0530

    feat: add homepage (functional)

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,6 @@
 // controller methods definition
 function welcomeMessage(req, res){
-    res.send('<h3 style="text-align:center; font-size: 2.5em"><i>Welcome to <span style="color:blue;">The Social Network</span></i></h3>')
+    res.render('homepage')
 }
 
 function loginPage(req, res) {
