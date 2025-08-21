c59eee8ec7605e71fb040749dd08f50deb12ce60
abythomas300 <abythomas300@gmail.com>
2025-08-21 20:41:54 +0530

    feat(views): display all blog details in admin panel

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,13 +1,27 @@
+const postModel = require('../models/post')
+
+
+
 function displayAdminHomepage(req, res) {
-
     res.render('homepage_admin')
-
 }
 
-function displayAllBlogs(req, res) {
-    
-    res.render('blogsListPage_admin')
+async function displayAllBlogs(req, res) {
 
+    try{
+        const allBlogs = await postModel.find({}).populate('author')
+        console.log("ALL FETCHED BLOGS:")
+        console.log(allBlogs)
+
+        if(allBlogs.length === 0){
+            res.send("No posts available for this moment")
+        }else{
+            res.render('blogsListPage_admin', {blogs: allBlogs})
+        }
+    }catch(error){
+        console.log("Failed to fetch blogs from DB, reason: ", error)
+        res.render("Error displaying blogs, try again.")
+    }
 }
 
 function displayUsersList(req, res) {
