390bc4daff422f94e59e865453384649ddb73a74
abythomas300 <abythomas300@gmail.com>
2025-08-22 23:49:26 +0530

    fix(view): typo in blog creation page

--- controllers/postController.js
+++ controllers/postController.js
@@ -43,6 +43,7 @@ async function createNewPost(req, res) {
         console.log("Saving post to DB ...")
         await newPost.save() // saving the post to 'posts' collection in DB
         console.log("Post created Successfully")
+        console.log("Uploaded File Details: ", req.file)
         req.flash('success', 'Blog post created successfully')
         res.redirect('/post')  // redirecting user to blogs page after creating a post
     }
