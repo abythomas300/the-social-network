c12403fdc83a8c9b670bd1373bc9d2ea8b8ce3c8
abythomas300 <abythomas300@gmail.com>
2025-08-22 23:07:48 +0530

    config: multer middleware with custom configuration for disk storage

---
+++ middlewares/multer-config.js
@@ -0,0 +1,22 @@
+// configuration for multer package
+const multer = require('multer')
+
+// custom configuration for uploaded file storage
+const storageConfig = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, './uploads')
+    },
+    filename: function(req, file, cb){
+        const uniqueFileName = `${file.fieldname}-${Date.now()}-${file.originalname}`
+        cb(null, uniqueFileName)
+    }
+})
+
+// creating a multer instance with custom storage configuration
+const upload = multer({storage:storageConfig})
+
+
+
+// exporting the created multer instance
+module.exports = upload
+
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 // importing middlewares
 const isLoggedIn = require('../middlewares/isLoggedIn')
+const upload = require('../middlewares/multer-config')
 
 // importing contoller
 const postController = require('../controllers/postController')
@@ -12,7 +13,7 @@ const postController = require('../controllers/postController')
 // route handlers
 router.get('/', isLoggedIn, postController.getAllPosts)
 router.get('/newblog', isLoggedIn, postController.showBlogCreationPage)
-router.post('/', isLoggedIn, postController.createNewPost)
+router.post('/', isLoggedIn, upload.single('blogThumbail'), postController.createNewPost)
 router.delete('/:blogId', isLoggedIn, postController.deletePost)
 router.get('/editBlog/:blogId', isLoggedIn, postController.showBlogEditPage )
 router.patch('/:blogId', isLoggedIn, postController.updatePost)
