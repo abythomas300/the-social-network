4c59cbe22101ccd6b69341cc9d587668f4213bfc
abythomas300 <abythomas300@gmail.com>
2026-03-11 14:06:06 +0530

    fix(views): switch to alternate profile placeholder API (fixes #3)

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(helmet({
         directives: {
             "default-src": ["'self'"],
             "script-src": ["'self'", "https://cdn.jsdelivr.net", (req, res) => `'nonce-${res.locals.cspNonce}'`],
-            "img-src": ["'self'", "https://i.pravatar.cc", "https://avatar.iran.liara.run"],
+            "img-src": ["'self'", "https://api.dicebear.com"],
             "connect-src": ["'self'", "https://cdn.jsdelivr.net"]
         }
     }
